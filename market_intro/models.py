17415a102e886f0f7702a8c880190539eb16207a
Jacopo Magnani <36220363+jacopomagnani@users.noreply.github.com>
2023-09-20 14:31:33 +0200

    finalized quiz

--- market_intro/models.py
+++ market_intro/models.py
@@ -9,7 +9,6 @@ from otree.api import (
     currency_range,
 )
 
-
 author = 'Your name here'
 
 doc = """
@@ -20,7 +19,56 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'market_intro'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 7
+    questions_list = [{'q1': 'There are two safe assets (X and Y).',
+                       'q2': 'There are three risky assets (X, Y and Z).',
+                       'q3': 'There are two safe assets (X and Y) and a risky one (Z).',
+                       'q4': 'There are two risky assets (X and Y) and a safe one (Z).',
+                       'correct_answer': 4
+                       },
+                      {
+                          'q1': 'The payoffs of the risky assets are not related.',
+                          'q2': 'When the payoffs of Asset A is high then the payoff of Asset B is low.',
+                          'q3': 'When the payoffs of Asset A is high then the payoff of Asset B is high.',
+                          'q4': 'When the payoffs of Asset A is low then the payoff of Asset B is low.',
+                          'correct_answer': 1
+                      },
+                      {
+                          'q1': 'Your endowment in a round depends on your earnings in the previous round.',
+                          'q2': 'Each round you start with a new endowment of cash and shares.',
+                          'q3': 'Your cash in a round is impacted by your trading in the previous round.',
+                          'q4': 'Your endowment in a round depends on the asset payoffs in the previous round.',
+                          'correct_answer': 2
+                      },
+                      {
+                          'q1': 'A trade occurs when a trader enters a bid greater than or equal to the lowest ask, or enters an ask lower than or equal to the highest bid.',
+                          'q2': 'A trade occurs when a trader enters a bid lower than or equal to the lowest ask, or  enters an ask lower than or equal to the highest bid.',
+                          'q3': 'A trade can only occur when a trader enters a bid that is equal to an ask.',
+                          'q4': 'A trade can only occur when at least a bid and an ask have already been posted.',
+                          'correct_answer': 1
+                      },
+                      {
+                          'q1': 'A trade occurs when a trader clicks on a number in the trades column.',
+                          'q2': 'A trade occurs when a trader clicks on Bid.',
+                          'q3': 'A trade occurs when a trader clicks on Ask.',
+                          'q4': 'A trade occurs when a trader double-clicks on a current ask or bid.',
+                          'correct_answer': 4
+                      },
+                      {
+                          'q1': 'Your available and settled amount of assets are always equal.',
+                          'q2': 'Your settled amount of assets decreases when you place an ask.',
+                          'q3': 'Your available amount of assets decreases when you place an ask.',
+                          'q4': 'Your available amount of assets decreases when you place a bid.',
+                          'correct_answer': 3
+                      },
+                      {
+                          'q1': 'Your available and settled amount of cash are always equal.',
+                          'q2': 'Your available cash decreases when you place a bid whereas your settled cash stays the same.',
+                          'q3': 'Your settled cash decreases when you place a bid whereas your available cash stays the same.',
+                          'q4': 'Your available cash increases when you place a bid.',
+                          'correct_answer': 2
+                      }
+                      ]
 
 
 class Subsession(BaseSubsession):
@@ -32,13 +80,15 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-
-    answer1 = models.IntegerField(
-        choices=[
-            [1, '150'],
-            [2, '200'],
-            [3, '250'],
-            [4, '300']
-        ],
+    answer = models.IntegerField(
         widget=widgets.RadioSelect
     )
+
+    num_correct = models.IntegerField()
+
+    def get_outcome(self):
+        k = 0
+        for i in range(1, Constants.num_rounds):
+            if self.in_round(i).answer == Constants.questions_list[i-1]["correct_answer"]:
+                k = k + 1
+        self.num_correct = k
--- market_intro/pages.py
+++ market_intro/pages.py
@@ -4,21 +4,58 @@ from .models import Constants
 
 
 class Instructions(Page):
-    pass
+    def is_displayed(self):
+        if self.subsession.round_number == 1:
+            return True
+        else:
+            return False
 
 
 class Quiz(Page):
 
     form_model = 'player'
-    form_fields = ['answer1']
+    form_fields = ['answer']
+
+    def answer_choices(self):
+
+        choices = [[1, Constants.questions_list[self.subsession.round_number-1]["q1"]],
+                   [2, Constants.questions_list[self.subsession.round_number-1]["q2"]],
+                   [3, Constants.questions_list[self.subsession.round_number-1]["q3"]],
+                   [4, Constants.questions_list[self.subsession.round_number-1]["q4"]], ]
+        return choices
+
+    def before_next_page(self):
+        if self.subsession.round_number == Constants.num_rounds:
+            self.player.get_outcome()
 
 
 class Results(Page):
-    pass
+
+    def is_displayed(self):
+        if self.subsession.round_number == Constants.num_rounds:
+            return True
+        else:
+            return False
+
+    def vars_for_template(self):
+        questions = []
+        i = 0
+        for x in Constants.questions_list:
+            i = i+1
+            y = [x["q1"], x["q2"], x["q3"], x["q4"], x["correct_answer"], self.player.in_round(i).answer]
+            questions.append(y)
+
+        return {
+            'questions': questions
+        }
 
 
 class MyWaitPage(WaitPage):
-    pass
+    def is_displayed(self):
+        if self.subsession.round_number == Constants.num_rounds:
+            return True
+        else:
+            return False
 
 
 page_sequence = [Instructions, Quiz, Results, MyWaitPage]
