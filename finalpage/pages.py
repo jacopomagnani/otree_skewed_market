f80fced5a09f07d116b33943c64fa1859903b15b
Jacopo Magnani <36220363+jacopomagnani@users.noreply.github.com>
2023-10-05 21:50:48 +0200

    cleaned up payoffs

--- finalpage/pages.py
+++ finalpage/pages.py
@@ -6,16 +6,14 @@ import decimal
 
 class MyPage(Page):
     def vars_for_template(self):
-        # final_pay = decimal.Decimal(self.participant.payoff_plus_participation_fee)
-        # final_pay = final_pay.quantize(decimal.Decimal('0.01'))
-        # final_pay = final_pay.quantize(decimal.Decimal('0.1'), rounding='ROUND_UP')
-        # final_pay = final_pay + self.session.config['participation_fee']
+
+        total_score = self.participant.vars['quiz_score'] + self.participant.vars['market_score'] + self.participant.vars['mpl_score']
+
         return {
-            'rate': self.session.config['real_world_currency_per_point'],
             'fee': self.session.config['participation_fee'],
-            'total_payoff_in_currency': self.participant.payoff.to_real_world_currency(self.session),
-            'raw_payment': self.participant.payoff_plus_participation_fee,
-            # 'final_payment': final_pay
+            'score': total_score,
+            'score_in_currency': self.participant.payoff.to_real_world_currency(self.session),
+            'payment': self.participant.payoff_plus_participation_fee,
         }
 
 
